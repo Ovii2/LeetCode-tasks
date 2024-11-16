a5e826d10eca15ca0be0d3587872336254ee8aea
Ovidijus <ovidijus.e@gmail.com>
2024-11-16 09:28:59 +0200

    Remove element from array

---
+++ 27.Remove element/src/main/java/org/example/company/Main.java
@@ -0,0 +1,8 @@
+package org.example.company;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.removeElement(new int[]{3, 2, 2, 3}, 3));
+    }
+}
