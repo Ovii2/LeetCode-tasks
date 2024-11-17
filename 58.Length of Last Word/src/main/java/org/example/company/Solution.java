292d1a4d7a883e723769ab49221727463f9cb4d5
Ovidijus <ovidijus.e@gmail.com>
2024-11-17 18:32:52 +0200

    Length of Last Word

---
+++ 58.Length of Last Word/src/main/java/org/example/company/Solution.java
@@ -0,0 +1,8 @@
+package org.example.company;
+
+public class Solution {
+    public int lengthOfLastWord(String s) {
+        String[] parts = s.trim().split(" ");
+        return parts[parts.length - 1].length();
+    }
+}
