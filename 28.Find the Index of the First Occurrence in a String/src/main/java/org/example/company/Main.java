f3b690b60c3a5e22f9007eb8988602a9a288aa89
Ovidijus <ovidijus.e@gmail.com>
2024-11-17 08:56:56 +0200

    Find the Index of the First Occurrence in a String

---
+++ 28.Find the Index of the First Occurrence in a String/src/main/java/org/example/company/Main.java
@@ -0,0 +1,10 @@
+package org.example.company;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.strStr("sadbutsad", "sad"));
+        System.out.println(solution.strStr("leetcode", "leeto"));
+        System.out.println(solution.strStr("hello", "ll"));
+    }
+}
---
+++ 28.Find the Index of the First Occurrence in a String/src/main/java/org/example/company/Solution.java
@@ -0,0 +1,10 @@
+package org.example.company;
+
+public class Solution {
+    public int strStr(String haystack, String needle) {
+        if (needle.isEmpty()) {
+            return 0;
+        }
+        return haystack.indexOf(needle);
+    }
+}
