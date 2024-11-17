a16746150d0a2f1d9ba0ba9d5fa18e2192bb57c3
Ovidijus <ovidijus.e@gmail.com>
2024-11-17 09:12:34 +0200

    Search Insert Position

---
+++ 35.Search Insert Position/src/main/java/org/example/company/Main.java
@@ -0,0 +1,10 @@
+package org.example.company;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.searchInsert(new int[]{1, 3, 5, 6}, 5));
+        System.out.println(solution.searchInsert(new int[]{1, 3, 5, 6}, 7));
+
+    }
+}
