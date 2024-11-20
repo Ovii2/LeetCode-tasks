b8a64c0b2f60161005b1f76eda57428c4cac2177
Ovidijus <ovidijus.e@gmail.com>
2024-11-20 07:11:23 +0200

    Remove duplicates from sorted list

---
+++ 83.Remove duplicates from sorted list/src/main/java/org/example/company/Main.java
@@ -0,0 +1,7 @@
+package org.example.company;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ 83.Remove duplicates from sorted list/src/main/java/org/example/company/Solution.java
@@ -0,0 +1,17 @@
+package org.example.company;
+
+public class Solution {
+
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode result = head;
+
+        while (head != null && head.next != null) {
+            if (head.val == head.next.val) {
+                head.next = head.next.next;
+            } else {
+                head = head.next;
+            }
+        }
+        return result;
+    }
+}
