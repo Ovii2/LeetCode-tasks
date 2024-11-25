dc64ed3b02755527f07483644a01260e53710a5c
Ovidijus <ovidijus.e@gmail.com>
2024-11-25 08:54:49 +0200

    Maximum Depth of Binary Tree

---
+++ 104.Maximum Depth of Binary Tree/src/main/java/org/example/company/Solution.java
@@ -0,0 +1,8 @@
+package org.example.company;
+
+public class Solution {
+
+    public int maxDepth(TreeNode root) {
+        return root == null ? 0 : 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+}
