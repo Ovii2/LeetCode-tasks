b6b69a9c7849cc3518200e8e9ffc53660e0be0be
Ovidijus <ovidijus.e@gmail.com>
2024-11-26 09:48:33 +0200

    Fizz Buzz

---
+++ 412. Fizz Buzz/src/main/java/org/example/company/Main.java
@@ -0,0 +1,10 @@
+package org.example.company;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.fizzBuzz(3));
+        System.out.println(solution.fizzBuzz(5));
+        System.out.println(solution.fizzBuzz(15));
+    }
+}
