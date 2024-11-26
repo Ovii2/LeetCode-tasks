95a7718204a68200c83a7d205aa055c8b16dcad1
Ovidijus <ovidijus.e@gmail.com>
2024-11-26 20:10:02 +0200

    Balanced Binary Tree

---
+++ 110.Balanced Binary Tree/src/main/java/org/example/company/Solution.java
@@ -0,0 +1,27 @@
+package org.example.company;
+
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        return height(root) != -1;
+    }
+
+    public int height(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int leftHeight = height(root.left);
+        int rightHeight = height(root.right);
+
+        if (leftHeight == -1 || rightHeight == -1) {
+            return -1;
+        }
+
+        if (Math.abs(leftHeight - rightHeight) > 1) {
+            return -1;
+        }
+
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+}
+
