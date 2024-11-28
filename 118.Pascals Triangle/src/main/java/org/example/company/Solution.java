020b279b47ce456e171d804c8ac25787e862ec6e
Ovidijus <ovidijus.e@gmail.com>
2024-11-28 09:41:20 +0200

    Pascals triangle

---
+++ 118.Pascals Triangle/src/main/java/org/example/company/Solution.java
@@ -0,0 +1,21 @@
+package org.example.company;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        for (int i = 0; i < numRows; i++) {
+            List<Integer> row = new ArrayList<>();
+            int number = 1;
+            for (int j = 0; j <= i; j++) {
+                row.add(number);
+                number = number * (i - j) / (j + 1);
+            }
+            result.add(row);
+        }
+        return result;
+    }
+}
