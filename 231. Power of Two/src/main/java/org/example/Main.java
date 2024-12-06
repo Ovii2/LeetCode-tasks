4fbe92da2e3f45bed6a35d21df27f4d4ad173350
Ovidijus <ovidijus.e@gmail.com>
2024-12-06 14:45:57 +0200

    Power of Two

---
+++ 231. Power of Two/src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.isPowerOfTwo(1));
+        System.out.println(solution.isPowerOfTwo(16));
+        System.out.println(solution.isPowerOfTwo(3));
+    }
+}
---
+++ 231. Power of Two/src/main/java/org/example/Solution.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Solution {
+    public boolean isPowerOfTwo(int n) {
+        return n > 0 && Integer.bitCount(n) == 1;
+    }
+}
