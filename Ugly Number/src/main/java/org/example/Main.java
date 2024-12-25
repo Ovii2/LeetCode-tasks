1fce8948453a41546395cfb3806a403f8a9961e0
Ovidijus <ovidijus.e@gmail.com>
2024-12-25 10:39:40 +0200

    Ugly number

---
+++ Ugly Number/src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.isUgly(6));
+        System.out.println(solution.isUgly(1));
+        System.out.println(solution.isUgly(14));
+    }
+}
