7df6a085403a89aa034d0ac24ba6ff433bee0d74
Ovidijus <ovidijus.e@gmail.com>
2026-01-05 09:32:01 +0200

    Multiply Strings

---
+++ 43. Multiply Strings/src/main/java/com/example/org/Solution.java
@@ -0,0 +1,9 @@
+package com.example.org;
+
+import java.math.BigInteger;
+
+public class Solution {
+    public String multiply(String num1, String num2) {
+        return new BigInteger(num1).multiply(new BigInteger(num2)).toString();
+    }
+}
