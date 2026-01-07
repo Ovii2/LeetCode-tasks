4e3b4690e24dd2b4d63e13ba8af937483dac469d
Ovidijus <ovidijus.e@gmail.com>
2026-01-07 10:32:24 +0200

    122. Best Time to Buy and Sell Stock II

---
+++ 122. Best Time to Buy and Sell Stock II/src/main/java/com/example/org/Main.java
@@ -0,0 +1,9 @@
+package com.example.org;
+
+public class Main {
+    static void main() {
+        Solution solution = new Solution();
+        IO.println(solution.maxProfit(new int[]{7, 1, 5, 3, 6, 4}));
+        IO.println(solution.maxProfit(new int[]{1, 2, 3, 4, 5}));
+    }
+}
